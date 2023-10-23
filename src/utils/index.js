6c78ab8344b5d865765042e86233a93cd93892f5
Liang Yue <2268235855@qq.com>
2023-10-23 23:50:06 +1300

    second

---
+++ src/utils/index.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+export const isFalsy = (value) => (value === 0 ? false : !value);
+export const cleanObject = (object) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// const debounce = (func, delay) => {
+//   let timeout;
+//   return () => {
+//     if (timeout) {
+//       clearTimeout(timeout);
+//     }
+//     timeout = setTimeout(function () {
+//       func();
+//     }, delay);
+//   };
+// };
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
+};
